657fca346a466013ef74da434c6439d552fe2af6
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:54:43 +0530

    Update ProductRepository.java

--- src/main/java/com/suryansh/groceryapp/Repository/ProductRepository.java
+++ src/main/java/com/suryansh/groceryapp/Repository/ProductRepository.java
@@ -1,4 +1,89 @@
 package com.suryansh.groceryapp.Repository;
 
+import com.suryansh.groceryapp.Model.Product;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductRepository {
+    private static final String URL = "jdbc:mysql://localhost:3306/grocerydb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "*****";
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+
+    public boolean addProduct(Product product) {
+        String sql = "INSERT INTO products (name, category, price, stock_quantity) VALUES (?, ?, ?, ?)";
+        try (Connection connection = getConnection();
+             PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+            stmt.setString(1, product.getName());
+            stmt.setString(2, product.getCategory());
+            stmt.setDouble(3, product.getPrice());
+            stmt.setInt(4, product.getStockQuantity());
+
+            int rows = stmt.executeUpdate();
+            return rows > 0;
+        } catch (SQLException e) {
+            System.out.println("Error Adding product " + e.getMessage());
+            return false;
+        }
+    }
+
+    public List<Product> getallProduct(){
+        List<Product> products=new ArrayList<>();
+        String sql="SELECT * FROM products";
+        try (Connection connection = getConnection();
+             PreparedStatement stmt = connection.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                Product product = new Product();
+                product.setId(rs.getInt("id"));
+                product.setName(rs.getString("name"));
+                product.setCategory(rs.getString("category"));
+                product.setPrice(rs.getDouble("price"));
+                product.setStockQuantity(rs.getInt("stock_quantity"));
+                products.add(product);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error retrieving products: " + e.getMessage());
+        }
+
+
+        return products;
+    }
+
+    public boolean updateProduct(Product product) {
+        String sql = "UPDATE products SET name = ?, price = ?, quantity = ? WHERE id = ?";
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, product.getName());
+            stmt.setDouble(2, product.getPrice());
+            stmt.setInt(3, product.getStockQuantity());
+            stmt.setInt(4, product.getId());
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            System.out.println("Error updating product: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean deleteProduct(int productId) {
+        String sql = "DELETE FROM products WHERE id = ?";
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, productId);
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            System.out.println("Error deleting product: " + e.getMessage());
+            return false;
+        }
+    }
+
 }
