a99f03947bf3812c6e9d902181fe99dfa51f1f64
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:55:50 +0530

    Update CustomerService.java

--- src/main/java/com/suryansh/groceryapp/Services/CustomerService.java
+++ src/main/java/com/suryansh/groceryapp/Services/CustomerService.java
@@ -1,4 +1,21 @@
 package com.suryansh.groceryapp.Services;
 
+import com.suryansh.groceryapp.Model.Customer;
+import com.suryansh.groceryapp.Repository.CustomerRepository;
+
 public class CustomerService {
+    private CustomerRepository customerRepository=new CustomerRepository();
+    public boolean registerCustomer(Customer customer){
+        return customerRepository.registerCustomer(customer);
+    }
+
+    public Customer login(String email,String password){
+        return customerRepository.findbyemailandpassword(email, password);
+    }
+
+    public boolean updateCustomer(Customer customer){
+
+        return CustomerRepository.updateCustomer(customer);
+    }
 }
+
