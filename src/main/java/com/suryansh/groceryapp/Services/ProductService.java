4d82fc3b11738baf1fb70291ff7fd6eddaa5dc84
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:56:09 +0530

    Update ProductService.java

--- src/main/java/com/suryansh/groceryapp/Services/ProductService.java
+++ src/main/java/com/suryansh/groceryapp/Services/ProductService.java
@@ -1,4 +1,27 @@
 package com.suryansh.groceryapp.Services;
 
+import com.suryansh.groceryapp.Model.Product;
+import com.suryansh.groceryapp.Repository.ProductRepository;
+
+import java.util.List;
+
 public class ProductService {
+    private ProductRepository productRepository=new ProductRepository();
+    public List<Product> getallProducts(){
+        return productRepository.getallProduct();
+    }
+
+    public boolean addProduct(Product product){
+        return productRepository.addProduct(product);
+    }
+
+    public boolean updateProduct(Product product) {
+        return productRepository.updateProduct(product);
+    }
+
+    public boolean deleteProduct(int productId) {
+        return productRepository.deleteProduct(productId);
+    }
+
 }
+
