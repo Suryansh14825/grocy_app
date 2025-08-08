626d09da88113677b1c2a7387155e79511dc9893
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:56:31 +0530

    Update GroceryApp.java

--- src/main/java/com/suryansh/groceryapp/GroceryApp.java
+++ src/main/java/com/suryansh/groceryapp/GroceryApp.java
@@ -1,4 +1,11 @@
 package com.suryansh.groceryapp;
 
+import com.suryansh.groceryapp.Ui.CustomerMenu;
+
 public class GroceryApp {
+    public static void main(String[] args) {
+        CustomerMenu menu = new CustomerMenu();
+        menu.start();
+    }
 }
+
