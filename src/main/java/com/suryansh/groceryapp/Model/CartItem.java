27396cde9664c1c5912ca944bb86feb211b65f86
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:51:39 +0530

    Update CartItem.java

--- src/main/java/com/suryansh/groceryapp/Model/CartItem.java
+++ src/main/java/com/suryansh/groceryapp/Model/CartItem.java
@@ -1,4 +1,35 @@
 package com.suryansh.groceryapp.Model;
 
 public class CartItem {
+    private int id;
+    private int customerId;
+    private int productId;
+    private int quantity;
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+    public int getProductId() {
+        return productId;
+    }
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
+
