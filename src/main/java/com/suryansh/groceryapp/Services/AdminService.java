973667309cc5e6c5da994cff5efac9c0339f9879
Suryansh14825 <105674994+Suryansh14825@users.noreply.github.com>
2025-08-08 20:55:06 +0530

    Update AdminService.java

--- src/main/java/com/suryansh/groceryapp/Services/AdminService.java
+++ src/main/java/com/suryansh/groceryapp/Services/AdminService.java
@@ -1,4 +1,13 @@
 package com.suryansh.groceryapp.Services;
 
+import com.suryansh.groceryapp.Model.Admin;
+import com.suryansh.groceryapp.Repository.AdminRepository;
+
 public class AdminService {
+    private AdminRepository adminRepo = new AdminRepository();
+
+    public Admin login(String username, String password) {
+        return adminRepo.login(username, password);
+    }
 }
+
